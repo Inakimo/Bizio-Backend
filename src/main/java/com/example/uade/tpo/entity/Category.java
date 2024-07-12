2c3882fb6948ea090a233cd5463a0a40c3ee0334
Iñaki Moreno <99410656+Inakimo@users.noreply.github.com>
2024-07-12 15:19:43 -0300

    Bizio-backend

---
+++ src/main/java/com/example/uade/tpo/entity/Category.java
@@ -0,0 +1,28 @@
+package com.example.uade.tpo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categories_seq")
+    @SequenceGenerator(name = "categories_seq", sequenceName = "categories_seq", allocationSize = 1)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+}
